60df32eddbfaffe5a2d7b3bfcd5c5c33983fef6d
manikandan031 <manikandan031@gmail.com>
2018-03-29 17:00:45 +0530

    Separated Controller and View

--- src/components/author/authorPage.js
+++ src/components/author/authorPage.js
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
 
 var Authors = React.createClass({
     getInitialState: function(){
@@ -9,33 +10,19 @@ var Authors = React.createClass({
             authors: []
         };
     },
-    componentWillMount: function(){
-        this.setState({
-            authors: AuthorApi.getAllAuthors()
-        });
+    componentDidMount: function(){
+        if(this.isMounted()){
+            this.setState({
+                authors: AuthorApi.getAllAuthors()
+            });
+        }
     },
     render: function(){
-        var createAuthorRow = function(author){
-            return (
-                <tr key={author.id}>
-                    <td><a href={"/#author/" + author.id}>{author.id}</a></td>
-                    <td>{author.firstName} {author.lastName}</td>
-                </tr>
-            );
-        };
         return (
             <div>
                 <h1>Authors</h1>
-                <table className="table">
-                    <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </thead>
-                    <tbody>
-                        {this.state.authors.map(createAuthorRow, this)}
-                    </tbody>
-                </table>
-            </div>
+                <AuthorList authors={this.state.authors} />
+            </div> 
         );
     }
 });
