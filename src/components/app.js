a3a5c5933e28166e82cf6bd6cbd5e873d6dc301d
manikandan031 <manikandan031@gmail.com>
2018-03-29 20:33:09 +0530

    Added react-router routing

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+/*eslint-disable strict */ //Disabling eslint because we can't run in strict mode due to globals
+$ = jQuery = require('jquery');
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <Header />
+                <RouteHandler />
+            </div>
+        );
+    }
+});
+
+module.exports = App;
--- src/main.js
+++ src/main.js
@@ -1,41 +1,8 @@
-$ = jQuery = require('jquery');
+"use strict";
 var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
-var Authors = require('./components/author/authorPage');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win){
-    "use strict";
-    var App = React.createClass({
-        render: function(){
-            var Child;
-            switch(this.props.route){
-                case 'authors': 
-                    Child = Authors;
-                    break;
-                case 'about': 
-                    Child = About;
-                    break;
-                default:
-                    Child = Home;
-            }
-            return (
-                <div>
-                    <Header/>
-                    <Child/>
-                </div>
-            );
-        }
-    });
-
-function render(){
-    console.log('Hash: ' + win.location.hash);
-    var url = win.location.hash.substr(1);
-    React.render(<App route={url} />, document.getElementById('app'));
-}
-
-win.addEventListener('hashchange', render);
-render();
-
-})(window);
+Router.run(routes, function(Handler){
+    React.render(<Handler />, document.getElementById('app'));
+});
